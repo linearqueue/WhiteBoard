2278297d304eabf0697900c30c5e242110ed2f24
linearqueue <24204031154@stu.manit.ac.in>
2026-06-15 15:21:32 +0530

    user & canvas both working

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import connectDB from "./Database/connectDB.js";
 import userRouter from "./Routes/userRoute.js";
+import canvasRouter from "./Routes/canvasRoute.js";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRouter);
+app.use("/api/canvas", canvasRouter);
 
 app.get("/", (req, res) => {
   res.send("Working Fine");
